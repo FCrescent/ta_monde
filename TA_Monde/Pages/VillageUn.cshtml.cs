a442864984a627e58df09efea382ca137129931d
FCrescent <fabiencrescent04@gmail.com>
2021-06-12 15:02:50 +0200

    Premier commit

---
+++ TA_Monde/Pages/VillageUn.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TA_Monde.Pages
+{
+    public class VillageUnModel : PageModel
+    {
+        private readonly ILogger<VillageUnModel> _logger;
+
+        public VillageUnModel(ILogger<VillageUnModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ TA_Monde/wwwroot/js/village-un-01.js
@@ -0,0 +1,14 @@
+﻿
+
+//document.getElementById("plop").innerHTML = "Yattaaaaaaa!";
+
+class Character {
+    constructor(typeTa, appearX, appearY, happiness) {
+        this.typeTa = typeTa;
+        this.appearX = appearX;
+        this.appearY = appearY;
+        this.happiness = happiness;
+    }
+
+   
+}
